52bd259b43af2aa7bd05324954e8e1c211efbdfb
Kabillan T A <kabillan1905@gmail.com>
2026-03-09 11:39:46 +0530

    fix

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDWm-dJw_pHqQ5jmIyrV1VNodeSnnH0wrM",
-  authDomain: "kinetik-1234.firebaseapp.com",
-  projectId: "kinetik-1234",
-  storageBucket: "kinetik-1234.firebasestorage.app",
-  messagingSenderId: "169247644410",
-  appId: "1:169247644410:web:800455b254e06f551d4743",
-  measurementId: "G-5SLG2CDF2E",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
